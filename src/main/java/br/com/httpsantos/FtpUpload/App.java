0a03558741b18f10c1fd2556cf4198c418f77b9a
Carlos Henrique <carlos.hrq.rodrigues@gmail.com>
2019-03-02 16:00:11 -0300

    iniciando ftp

--- src/main/java/br/com/httpsantos/FtpUpload/App.java
+++ src/main/java/br/com/httpsantos/FtpUpload/App.java
@@ -1,13 +1,27 @@
 package br.com.httpsantos.FtpUpload;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+import java.io.IOException;
+import java.net.SocketException;
+
+import org.apache.commons.net.ftp.FTPClient;
+
+public class App {
+	public static void main(String[] args) throws SocketException, IOException {
+
+		FTPClient ftp = new FTPClient();
+
+		ftp.connect(""); // ftp.petrobras.com.br
+
+		ftp.login("usuario", "senha");
+
+		ftp.changeWorkingDirectory("diretorio");
+
+		String[] arq = ftp.listNames();
+
+		System.out.println("Listando arquivos: \n");
+
+		for (String f : arq) {
+			System.out.println(f);
+		}
+	}
 }
