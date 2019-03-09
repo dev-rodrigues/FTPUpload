f976cc009b6ca6839a9766f9bf4004aa2f1ee173
Carlos Henrique <carlos.hrq.rodrigues@gmail.com>
2019-03-09 11:41:40 -0300

    inicio refatoramento

--- src/main/java/br/com/httpsantos/FtpUpload/App.java
+++ src/main/java/br/com/httpsantos/FtpUpload/App.java
@@ -1,26 +1,23 @@
 package br.com.httpsantos.FtpUpload;
 
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.net.SocketException;
-
-import org.apache.commons.net.ftp.FTPClient;
 
 public class App {
 	public static void main(String[] args) throws SocketException, IOException {
 		
-		FTPClient ftp = new FTPClient();
-		
-		ftp.connect("192.168.0.18");
-
-		ftp.login("user", "123");
-
-		FileInputStream arqEnviar = new FileInputStream("C:\\temp\\out.txt");
-
-		if (ftp.storeFile("meuarquivo.txt", arqEnviar)) {
-			System.out.println("Arquivo armazenado com sucesso!");
-		} else {
-			System.out.println("Erro ao armazenar o arquivo.");
-		}
+//		FTPClient ftp = new FTPClient();
+//		
+//		ftp.connect("192.168.0.18");
+//
+//		ftp.login("user", "123");
+//
+//		FileInputStream arqEnviar = new FileInputStream("C:\\temp\\out.txt");
+//
+//		if (ftp.storeFile("meuarquivo.txt", arqEnviar)) {
+//			System.out.println("Arquivo armazenado com sucesso!");
+//		} else {
+//			System.out.println("Erro ao armazenar o arquivo.");
+//		}
 	}
 }
